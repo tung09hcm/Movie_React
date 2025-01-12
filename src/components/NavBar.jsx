fed433c1230d59d036789beb287e668c137a1dc9
tung <tung.bui2004lk@hcmut.edu.vn>
2025-01-13 01:16:30 +0700

    test3

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/Movie_React">Movie App</Link>
         </div>
         <div className="navbar-links">
             <Link to="/Movie_React" className="nav-link">Home</Link>
